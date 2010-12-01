ef13ef623ab13f2bb51f265dd02e5a51d1a683ce
Jeff Hammel <jhammel@mozilla.com>
2010-12-01 09:29:49 -0800

    add an --ignore option

--- manifestdestiny/convert.py
+++ manifestdestiny/convert.py
@@ -9,19 +9,24 @@ import sys
 from fnmatch import fnmatch
 from optparse import OptionParser
 
-def convert(directories, pattern=None):
+def convert(directories, pattern=None, ignore=()):
   retval = []
   for directory in directories:
     for dirpath, dirnames, filenames in os.walk(directory):
 
+      dirnames = []
+
       # reference only the subdirectory
-      dirpath = dirpath.split(directory, 1)[-1]
+      dirpath = dirpath.split(directory, 1)[-1].strip('/')
+
+      if dirpath.split(os.path.sep)[0] in ignore:
+        continue
 
       # filter by glob
       if pattern:
         filenames = [filename for filename in filenames
                      if fnmatch(filename, pattern)]
-      
+
       # add to the list
       retval.extend([os.path.join(dirpath, filename)
                      for filename in filenames])
@@ -34,6 +39,8 @@ def main(args=sys.argv[1:]):
   parser = OptionParser(usage=usage)
   parser.add_option('-p', '--pattern', dest='pattern',
                     help="glob pattern for files")
+  parser.add_option('-i', '--ignore', dest='ignore', action='append',
+                    help='directories to ignore')
   options, args = parser.parse_args(args)
   if not len(args):
     parser.print_usage()
@@ -41,7 +48,7 @@ def main(args=sys.argv[1:]):
   for arg in args:
     assert os.path.exists(arg)
     assert os.path.isdir(arg)
-  print convert(args, pattern=options.pattern)
+  print convert(args, pattern=options.pattern, ignore=options.ignore)
 
 if __name__ == '__main__':
   main()
