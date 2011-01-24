971e3d69ee4d3ae3f3556933d6f73c221d3579bd
Jeff Hammel <jhammel@mozilla.com>
2011-01-24 13:40:26 -0800

    stub out everything for copying except copying

--- manifestdestiny/copy.py
+++ manifestdestiny/copy.py
@@ -12,7 +12,7 @@ def copy(from_manifest, to_manifest, *tags, **kwargs):
     # parse the manifests
     assert os.path.exists(from_manifest), "'%s' does not exist"
     from_dir = os.path.dirname(os.path.abspath(from_manifest))
-    manifest = manifests.ManifestParser(manifests=(from_manifest))
+    manifest = manifests.ManifestParser(manifests=(from_manifest,))
 
     # tests to copy
     copy_tests = []
@@ -22,11 +22,25 @@ def copy(from_manifest, to_manifest, *tags, **kwargs):
     # destination
     if os.path.isdir(to_manifest):
         to_dir = os.path.abspath(to_manifest)
-        to_manifest = os.path.join(os.path.dirname(os.path.abspath(to_manifest)),
-                                   os.path.basename(from_manifest))
+        to_manifest = os.path.join(to_dir, os.path.basename(from_manifest))
     else:
+        if os.path.exists(to_manifest):
+            # if the manifest exists, overwrite the manifest (be careful!)
+            to_dir = os.path.dirname(os.path.abspath(to_manifest))
+            to_manifest = os.path.abspath(to_manifest)
+        else:
+            # assert that what is given is a file name
+            # (not a directory)
+            to_manifest = os.path.abspath(to_manifest)
+            to_dir = os.path.dirname(to_manifest)
+            if not os.path.exists(to_dir):
+                os.path.makedirs(to_dir)
+            else:
+                # sanity check
+                assert os.path.isdir(to_dir)
 
-        # if the manifest exists, overwrite the manifest (be careful!)
+    # copy the damn things
+    
 
 
 def main(args=sys.argv[1:]):
