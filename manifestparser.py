2b084ea2156ba2cd3a86eda0a761872580ade4e5
Jeff Hammel <jhammel@mozilla.com>
2011-05-25 10:21:39 -0700

    bump version for release

--- manifestparser.py
+++ manifestparser.py
@@ -52,7 +52,7 @@ import sys
 from fnmatch import fnmatch
 from optparse import OptionParser
 
-version = '0.3.2' # package version
+version = '0.4' # package version
 try:
     from setuptools import setup
 except ImportError:
