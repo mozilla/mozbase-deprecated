2f4205696116e40d9e0112a557c630efe476fb21
Jeff Hammel <jhammel@mozilla.com>
2011-06-26 11:06:39 -0700

    add a relpath for old python

--- manifestparser.py
+++ manifestparser.py
@@ -66,7 +66,25 @@ except ImportError:
 try:
     relpath = os.path.relpath
 except AttributeError:
-    raise
+    def relpath(path, start):
+        """
+        Return a relative version of a path
+        from /usr/lib/python2.6/posixpath.py
+        """
+
+        if not path:
+            raise ValueError("no path specified")
+
+        start_list = os.path.abspath(start).split(os.path.sep)
+        path_list = os.path.abspath(path).split(os.path.sep)
+
+        # Work out how much of the filepath is shared by start and path.
+        i = len(os.path.commonprefix([start_list, path_list]))
+
+        rel_list = [pardir] * (len(start_list)-i) + path_list[i:]
+        if not rel_list:
+            return start
+        return join(*rel_list)
 
 # expr.py
 # from:
