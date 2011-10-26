3fbea12776b9dea39f7a16833c1ed83bb4bb95dc
Jeff Hammel <jhammel@mozilla.com>
2011-10-26 12:30:58 -0700

    Bug 697344 - add ability to MozHttpd block on serving

--- mozhttpd/mozhttpd.py
+++ mozhttpd/mozhttpd.py
@@ -76,22 +76,31 @@ class MozHttpd(object):
         self.port = int(port)
         self.docroot = docroot
 
-    def start(self):
+    def start(self, block=False):
+        """
+        start the server.  If block is True, the call will not return.
+        If block is False, the server will be started on a separate thread that
+        can be terminated by a call to .stop()
+        """
 
         class MozRequestHandlerInstance(MozRequestHandler):
             docroot = self.docroot
 
         self.httpd = EasyServer((self.host, self.port), MozRequestHandlerInstance)
-        self.server = threading.Thread(target=self.httpd.serve_forever)
-        self.server.setDaemon(True) # don't hang on exit
-        self.server.start()
-        self.testServer()
+        if block:
+            self.httpd.serve_forever()
+        else:
+            self.server = threading.Thread(target=self.httpd.serve_forever)
+            self.server.setDaemon(True) # don't hang on exit
+            self.server.start()
         
     def testServer(self):
         fileList = os.listdir(self.docroot)
         filehandle = urllib.urlopen('http://%s:%s' % (self.host, self.port))
         data = filehandle.readlines()
         filehandle.close()
+
+        retval = True
 
         for line in data:
             found = False
@@ -106,7 +115,9 @@ class MozHttpd(object):
                             found = True
                 
                 if not found:
+                    retval = False
                     print >> sys.stderr, "NOT FOUND: " + webline.strip()
+        return retval
 
     def stop(self):
         if self.httpd:
@@ -129,14 +140,24 @@ def main(args=sys.argv[1:]):
     parser.add_option('-d', '--docroot', dest='docroot',
                       default=os.getcwd(),
                       help="directory to serve files from [DEFAULT: %default]")
+    parser.add_option('--test', dest='test',
+                      action='store_true', default=False,
+                      help='run the tests and exit')
     options, args = parser.parse_args(args)
     if args:
         parser.print_help()
         parser.exit()
 
-    # serve
-    server = MozHttpd(**options.__dict__)
-    server.start()
+    # create the server
+    kwargs = options.__dict__.copy()
+    test = kwargs.pop('test')
+    server = MozHttpd(**kwargs)
+
+    if test:
+        server.start()
+        server.testServer()
+    else:
+        server.start(block=True)
 
 if __name__ == '__main__':
     main()
