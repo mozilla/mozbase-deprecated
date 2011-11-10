98ee03e8004c4a42baf686806ca59d97cac4d821
Jeff Hammel <jhammel@mozilla.com>
2011-11-10 11:23:31 -0800

    Bug 700862 - include ps front-end to mozprocess;r=jmaher

--- mozprocess/mozprocess/pid.py
+++ mozprocess/mozprocess/pid.py
@@ -39,28 +39,79 @@
 #
 # ***** END LICENSE BLOCK *****
 
+import os
 import mozinfo
+import shlex
 import subprocess
+import sys
 
-def get_pids(name, minimun_pid=0):
-  """Get all the pids matching name, exclude any pids below minimum_pid."""
+# determine the platform-specific invocation of `ps`
+if mozinfo.isMac:
+    psarg = '-Acj'
+elif mozinfo.isLinux:
+    psarg = 'axwww'
+else:
+    psarg = 'ax'
+
+def ps(arg=psarg):
+    """
+    python front-end to `ps`
+    http://en.wikipedia.org/wiki/Ps_%28Unix%29
+    returns a list of process dicts based on the `ps` header
+    """
+    retval = []
+    process = subprocess.Popen(['ps', arg], stdout=subprocess.PIPE)
+    stdout, _ = process.communicate()
+    header = None
+    for line in stdout.splitlines():
+        line = line.strip()
+        if header is None:
+            # first line is the header
+            header = line.split()
+            continue
+        split = line.split(None, len(header)-1)
+        process_dict = dict(zip(header, split))
+        retval.append(process_dict)
+    return retval
+
+def running_processes(name, psarg=psarg, defunct=True):
+    """
+    returns a list of 
+    {'PID': PID of process (int)
+     'command': command line of process (list)}
+     with the executable named `name`.
+     - defunct: whether to return defunct processes
+    """
+    retval = []
+    for process in ps(psarg):
+        command = process['COMMAND']
+        command = shlex.split(command)
+        if command[-1] == '<defunct>':
+            command = command[:-1]
+            if not command or not defunct:
+                continue
+        if 'STAT' in process and not defunct:
+            if process['STAT'] == 'Z+':
+                continue
+        prog = command[0]
+        basename = os.path.basename(prog)
+        if basename == name:
+            retval.append((int(process['PID']), command))
+    return retval
+
+def get_pids(name):
+    """Get all the pids matching name"""
   
-  if mozinfo.isWin:
-      # use the windows-specific implementation
-      import wpk
-      pids = wpk.get_pids(name)
-  else:
-      process = subprocess.Popen(['ps', 'ax'], stdout=subprocess.PIPE)
-      output, _ = process.communicate()
-      data = output.splitlines()
-      pids = [int(line.split()[0]) for line in data if line.find(name) is not -1]
-
-  return [m for m in pids if m > minimun_pid]
+    if mozinfo.isWin:
+        # use the windows-specific implementation
+        import wpk
+        return wpk.get_pids(name)
+    else:
+        return [pid for pid,_ in running_processes(name)]
 
 if __name__ == '__main__':
-  import sys
-  pids = set()
-  for i in sys.argv[1:]:
-      pids.update(get_pids(i))
-  for i in sorted(pids):
-      print i
+    pids = set()
+    for i in sys.argv[1:]:
+        pids.update(get_pids(i))
+    for i in sorted(pids):
+        print i
