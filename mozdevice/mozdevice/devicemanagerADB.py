6c788565c42278aaba21d2174b80385e805eff79
William Lachance <wlach@mozilla.com>
2011-12-01 17:05:37 -0500

    Sync devicemanager* with the versions in mozilla-central
    We'll just mirror the contents of these exactly for now

--- mozdevice/mozdevice/devicemanagerADB.py
+++ mozdevice/mozdevice/devicemanagerADB.py
@@ -1,42 +1,3 @@
-# ***** BEGIN LICENSE BLOCK *****
-# Version: MPL 1.1/GPL 2.0/LGPL 2.1
-#
-# The contents of this file are subject to the Mozilla Public License Version
-# 1.1 (the "License"); you may not use this file except in compliance with
-# the License. You may obtain a copy of the License at
-# http://www.mozilla.org/MPL/
-#
-# Software distributed under the License is distributed on an "AS IS" basis,
-# WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
-# for the specific language governing rights and limitations under the
-# License.
-#
-# The Original Code is device manager.
-#
-# The Initial Developer of the Original Code is
-#  The Mozilla Foundation.
-# Portions created by the Initial Developer are Copyright (C) 2011
-# the Initial Developer. All Rights Reserved.
-#
-# Contributor(s):
-#  Joel Maher <jmaher@mozilla.com>
-#  Clint Talbert <ctalbert@mozilla.com>
-#  Will Lachance <wlachance@mozilla.com>
-#
-# Alternatively, the contents of this file may be used under the terms of
-# either the GNU General Public License Version 2 or later (the "GPL"), or
-# the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
-# in which case the provisions of the GPL or the LGPL are applicable instead
-# of those above. If you wish to allow use of your version of this file only
-# under the terms of either the GPL or the LGPL, and not to allow others to
-# use your version of this file under the terms of the MPL, indicate your
-# decision by deleting the provisions above and replace them with the notice
-# and other provisions required by the GPL or the LGPL. If you do not delete
-# the provisions above, a recipient may use your version of this file under
-# the terms of any one of the MPL, the GPL or the LGPL.
-#
-# ***** END LICENSE BLOCK *****
-
 import subprocess
 from devicemanager import DeviceManager, DMError
 import re
@@ -295,11 +256,11 @@ class DeviceManagerADB(DeviceManager):
   #  failure: None
   def killProcess(self, appname):
     procs = self.getProcessList()
-    for proc in procs:
-      if (proc[1] == appname):
-        p = self.runCmd(["shell", "ps"])
+    for (pid, name, user) in procs:
+      if name == appname:
+        p = self.runCmdAs(["shell", "kill", pid])
         return p.stdout.read()
-      return None
+    return None
 
   # external function
   # returns:
@@ -331,7 +292,7 @@ class DeviceManagerADB(DeviceManager):
       f = open(localFile)
       ret = f.read()
       f.close()
-      return ret;
+      return ret;      
     except:
       return None
 
@@ -545,6 +506,12 @@ class DeviceManagerADB(DeviceManager):
     args.insert(0, "adb")
     return subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
+  def runCmdAs(self, args):
+    if self.useRunAs:
+      args.insert(1, "run-as")
+      args.insert(2, self.packageName)
+    return self.runCmd(args)
+
   def checkCmd(self, args):
     args.insert(0, "adb")
     return subprocess.check_call(args)
@@ -576,10 +543,10 @@ class DeviceManagerADB(DeviceManager):
       self.runCmd(["version"])
     except Exception as (ex):
       print "unable to execute ADB: ensure Android SDK is installed and adb is in your $PATH"
-
+    
   def isCpAvailable(self):
     # Some Android systems may not have a cp command installed,
-    # or it may not be executable by the user.
+    # or it may not be executable by the user. 
     data = self.runCmd(["shell", "cp"]).stdout.read()
     if (re.search('Usage', data)):
       return True
@@ -590,10 +557,10 @@ class DeviceManagerADB(DeviceManager):
   def verifyRunAs(self, packageName):
     # If a valid package name is available, and certain other
     # conditions are met, devicemanagerADB can execute file operations
-    # via the "run-as" command, so that pushed files and directories
+    # via the "run-as" command, so that pushed files and directories 
     # are created by the uid associated with the package, more closely
     # echoing conditions encountered by Fennec at run time.
-    # Check to see if run-as can be used here, by verifying a
+    # Check to see if run-as can be used here, by verifying a 
     # file copy via run-as.
     self.useRunAs = False
     devroot = self.getDeviceRoot()
@@ -610,4 +577,4 @@ class DeviceManagerADB(DeviceManager):
         self.useRunAs = True
       self.checkCmd(["shell", "rm", devroot + "/tmp/tmpfile"])
       self.checkCmd(["shell", "run-as", packageName, "rm", "-r", devroot + "/sanity"])
-
+      
