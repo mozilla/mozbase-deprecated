a48971b7cd835835efaeef88e3a7c8171b926174
Henrik Skupin <mail@hskupin.info>
2012-05-30 11:53:21 +0200

    Bug 758864 - If no destination folder is given for the installer on Windows use a sub folder. r=ctalbert

--- mozinstall/mozinstall/mozinstall.py
+++ mozinstall/mozinstall/mozinstall.py
@@ -7,6 +7,7 @@
 import mozinfo
 from optparse import OptionParser
 import os
+import shutil
 import subprocess
 import sys
 import tarfile
@@ -16,15 +17,21 @@ if mozinfo.isMac:
     from plistlib import readPlist
 
 
-_default_apps = ["firefox",
-                 "thunderbird",
-                 "fennec"]
+_default_apps = ['firefox',
+                 'thunderbird',
+                 'fennec']
 
 
 class InstallError(Exception):
     """
     Thrown when the installation fails. Includes traceback
     if available.
+    """
+
+
+class InvalidBinary(Exception):
+    """
+    Thrown when the binary cannot be found after the installation.
     """
 
 
@@ -46,29 +53,34 @@ def install(src, dest=None, apps=_default_apps):
     """
 
     src = os.path.realpath(src)
-    assert(os.path.isfile(src))
+    if not is_installer(src):
+        raise InvalidSource(src + ' is not a recognized file type ' +
+                                  '(zip, exe, tar.gz, tar.bz2 or dmg)')
 
     if not dest:
         dest = os.path.dirname(src)
+
+        # On Windows the installer doesn't create a sub folder in the
+        # destination folder and would clutter the source folder
+        if mozinfo.isWin and src.lower().endswith('.exe'):
+            filename = os.path.splitext(os.path.basename(src))[0]
+            dest = os.path.join(dest, filename)
 
     trbk = None
     try:
         install_dir = None
         if zipfile.is_zipfile(src) or tarfile.is_tarfile(src):
             install_dir = _extract(src, dest)[0]
-        elif mozinfo.isMac and src.lower().endswith(".dmg"):
+        elif src.lower().endswith('.dmg'):
             install_dir = _install_dmg(src, dest)
-        elif mozinfo.isWin and os.access(src, os.X_OK):
+        elif src.lower().endswith('.exe'):
             install_dir = _install_exe(src, dest)
-        else:
-            raise InvalidSource(src + " is not a recognized file type " +
-                                      "(zip, exe, tar.gz, tar.bz2 or dmg)")
-    except InvalidSource, e:
-        raise
+
     except Exception, e:
         cls, exc, trbk = sys.exc_info()
-        install_error = InstallError("Failed to install %s" % src)
+        install_error = InstallError('Failed to install %s' % src)
         raise install_error.__class__, install_error, trbk
+
     finally:
         # trbk won't get GC'ed due to circular reference
         # http://docs.python.org/library/sys.html#sys.exc_info
@@ -76,6 +88,25 @@ def install(src, dest=None, apps=_default_apps):
 
     if install_dir:
         return get_binary(install_dir, apps=apps)
+
+
+def is_installer(src):
+    """
+    Tests if the given file is a valid installer package (zip, exe, tar.gz,
+    tar.bz2 or dmg)
+
+    src - the path to the install file
+    """
+
+    src = os.path.realpath(src)
+    assert os.path.isfile(src), 'Installer has to be a file'
+
+    if mozinfo.isLinux:
+        return tarfile.is_tarfile(src)
+    elif mozinfo.isMac:
+        return src.lower().endswith('.dmg')
+    elif mozinfo.isWin:
+        return src.lower().endswith('.exe') or zipfile.is_zipfile(src)
 
 
 def _extract(src, dest=None, delete=False):
@@ -105,13 +136,13 @@ def _extract(src, dest=None, delete=False):
         else:
             for name in namelist:
                 filename = os.path.realpath(os.path.join(dest, name))
-                if name.endswith("/"):
+                if name.endswith('/'):
                     os.makedirs(filename)
                 else:
                     path = os.path.dirname(filename)
                     if not os.path.isdir(path):
                         os.makedirs(path)
-                    dest = open(filename, "wb")
+                    dest = open(filename, 'wb')
                     dest.write(bundle.read(name))
                     dest.close()
 
@@ -154,32 +185,32 @@ def _install_dmg(src, dest):
     Returns the list of top level files that were extracted
     """
 
-    proc = subprocess.Popen("hdiutil attach " + src,
-                            shell=True,
-                            stdout=subprocess.PIPE)
-
     try:
+        proc = subprocess.Popen('hdiutil attach %s' % src,
+                                shell=True,
+                                stdout=subprocess.PIPE)
+
         for data in proc.communicate()[0].split():
-            if data.find("/Volumes/") != -1:
+            if data.find('/Volumes/') != -1:
                 appDir = data
                 break
 
         for appFile in os.listdir(appDir):
-            if appFile.endswith(".app"):
+            if appFile.endswith('.app'):
                 appName = appFile
                 break
+
+        mounted_path = os.path.join(appDir, appName)
 
         dest = os.path.join(dest, appName)
         assert not os.path.isfile(dest)
 
-        if not os.path.isdir(dest):
-            os.makedirs(dest)
+        # copytree() would fail if dest already exists.
+        shutil.rmtree(dest, ignore_errors=True)
+        shutil.copytree(mounted_path, dest, False)
 
-        subprocess.call("cp -r " +
-                        os.path.join(appDir, appName, "*") + " " + dest,
-                        shell=True)
     finally:
-        subprocess.call("hdiutil detach " + appDir + " -quiet",
+        subprocess.call('hdiutil detach %s -quiet' % appDir,
                         shell=True)
 
     return dest
@@ -188,10 +219,13 @@ def _install_dmg(src, dest):
 def _install_exe(src, dest):
     # possibly gets around UAC in vista (still need to run as administrator)
 
-    os.environ['__compat_layer'] = "RunAsInvoker"
-    cmd = [src, "/S", "/D=" + os.path.realpath(dest)]
-    # check_call?
-    subprocess.call(cmd)
+    os.environ['__compat_layer'] = 'RunAsInvoker'
+    cmd = [src, '/I', '/D=%s' % os.path.realpath(dest)]
+
+    # As long as we support Python 2.4 check_call will not be available.
+    result = subprocess.call(cmd)
+    if not result is 0:
+        raise Exception('Execution of installer failed.')
 
     return dest
 
@@ -205,44 +239,56 @@ def get_binary(path, apps=_default_apps):
               or None if the binary cannot be found
     """
 
+    binary = None
+
     # On OS X we can get the real binary from the app bundle
     if mozinfo.isMac:
         plist = '%s/Contents/Info.plist' % path
         assert os.path.isfile(plist), '"%s" has not been found.' % plist
 
-        return os.path.join(path, "Contents/MacOS/",
-                            readPlist(plist)['CFBundleExecutable'])
+        binary = os.path.join(path, 'Contents/MacOS/',
+                              readPlist(plist)['CFBundleExecutable'])
 
-    if mozinfo.isWin:
-        apps = [app + ".exe" for app in apps]
+    else:
+        if mozinfo.isWin:
+            apps = [app + '.exe' for app in apps]
 
-    for root, dirs, files in os.walk(path):
-        for filename in files:
-            # os.access evaluates to False for some reason, so not using it
-            if filename in apps:
-                return os.path.realpath(os.path.join(root, filename))
+        for root, dirs, files in os.walk(path):
+            for filename in files:
+                # os.access evaluates to False for some reason, so not using it
+                if filename in apps:
+                    binary = os.path.realpath(os.path.join(root, filename))
+                    break
+
+    if not binary:
+        # TODO: It should never happen but if it's the case the installed
+        # files should be removed. It can be done once the uninstaller has
+        # been implemented by bug 757411. For now just throw an exception.
+        raise InvalidBinary('"%s" does not contain a valid binary.' % path)
+
+    return binary
 
 
 def cli(argv=sys.argv[1:]):
     parser = OptionParser()
-    parser.add_option("-s", "--source",
-                      dest="src",
-                      help="Path to installation file. "
-                           "Accepts: zip, exe, tar.bz2, tar.gz, and dmg")
-    parser.add_option("-d", "--destination",
-                      dest="dest",
+    parser.add_option('-s', '--source',
+                      dest='src',
+                      help='Path to installation file. '
+                           'Accepts: zip, exe, tar.bz2, tar.gz, and dmg')
+    parser.add_option('-d', '--destination',
+                      dest='dest',
                       default=None,
-                      help="[optional] Directory to install application into")
-    parser.add_option("--app", dest="app",
-                      action="append",
+                      help='[optional] Directory to install application into')
+    parser.add_option('--app', dest='app',
+                      action='append',
                       default=_default_apps,
-                      help="[optional] Application being installed. "
-                           "Should be lowercase, e.g: "
-                           "firefox, fennec, thunderbird, etc.")
+                      help='[optional] Application being installed. '
+                           'Should be lowercase, e.g: '
+                           'firefox, fennec, thunderbird, etc.')
 
     (options, args) = parser.parse_args(argv)
     if not options.src or not os.path.exists(options.src):
-        parser.error("Error: A valid source has to be specified.")
+        parser.error('Error: A valid source has to be specified.')
 
     # Run it
     if os.path.isdir(options.src):
@@ -253,5 +299,5 @@ def cli(argv=sys.argv[1:]):
     print binary
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     sys.exit(cli())
