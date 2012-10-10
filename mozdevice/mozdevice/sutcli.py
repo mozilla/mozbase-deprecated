50e146e6aa963e88007d5e77b6620969b3b89b34
William Lachance <wlachance@mozilla.com>
2012-10-10 11:03:26 -0400

    Bug 799935 - Refactor sut client utility to work with adb as well;r=mcote

--- mozdevice/mozdevice/sutcli.py
+++ mozdevice/mozdevice/sutcli.py
@@ -3,7 +3,7 @@
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
 """
-Command-line client to control a device with the SUTAgent software installed
+Command-line client to control a device
 """
 
 import os
@@ -11,11 +11,10 @@ import posixpath
 import StringIO
 import sys
 import textwrap
+import mozdevice
 from optparse import OptionParser
 
-from mozdevice import droid
-
-class SUTCli(object):
+class DMCli(object):
 
     def __init__(self, args=sys.argv[1:]):
         self.commands = { 'install': { 'function': self.install,
@@ -106,15 +105,10 @@ class SUTCli(object):
         if len(self.args) < 1:
             self.parser.error("must specify command")
 
-        if self.options.verbose:
-            droid.DroidSUT.debug = 4
-
-        if not self.options.deviceip and not self.options.hwid:
-            if not os.environ.get('TEST_DEVICE'):
-                self.parser.error("Must specify device ip in TEST_DEVICE or "
-                                  "with --remoteDevice option")
-            else:
-                self.options.deviceip = os.environ.get('TEST_DEVICE')
+        if self.options.dmtype == "sut" and not self.options.host and \
+                not self.options.hwid:
+            self.parser.error("Must specify device ip in TEST_DEVICE or "
+                              "with --host option with SUT")
 
         (command_name, command_args) = (self.args[0], self.args[1:])
         if command_name not in self.commands:
@@ -126,28 +120,57 @@ class SUTCli(object):
                 command['max_args'] and len(command_args) > command['max_args']:
             self.parser.error("Wrong number of arguments")
 
-        if self.options.hwid:
-            self.dm = droid.DroidConnectByHWID(self.options.hwid)
-        elif self.options.deviceip:
-            self.dm = droid.DroidSUT(self.options.deviceip,
-                                     port=int(self.options.deviceport))
+        self.dm = self.getDevice(dmtype=self.options.dmtype,
+                                 hwid=self.options.hwid,
+                                 host=self.options.host,
+                                 port=self.options.port)
         command['function'](*command_args)
 
     def add_options(self, parser):
-        parser.add_option("-r", "--remoteDevice", action="store",
-                          type="string", dest="deviceip",
-                          help="Device IP", default=None)
-        parser.add_option("-d", "--hwid", action="store",
-                          type="string", dest="hwid",
-                          help="HWID", default=None)
-        parser.add_option("-p", "--remotePort", action="store",
-                          type="int", dest="deviceport",
-                          help="SUTAgent port (defaults to 20701)",
-                          default=20701)
         parser.add_option("-v", "--verbose", action="store_true",
                           dest="verbose",
                           help="Verbose output from DeviceManager",
                           default = False)
+        parser.add_option("--host", action="store",
+                          type = "string", dest = "host",
+                          help = "Device hostname (only if using TCP/IP)",
+                          default=os.environ.get('TEST_DEVICE'))
+        parser.add_option("-p", "--port", action="store",
+                          type = "int", dest = "port",
+                          help = "Custom device port (if using SUTAgent or "
+                          "adb-over-tcp)", default=None)
+        parser.add_option("-m", "--dmtype", action="store",
+                          type = "string", dest = "dmtype",
+                          help = "DeviceManager type (adb or sut, defaults " \
+                              "to adb)", default=os.environ.get('DM_TRANS',
+                                                                'adb'))
+        parser.add_option("-d", "--hwid", action="store",
+                          type="string", dest="hwid",
+                          help="HWID", default=None)
+
+    def getDevice(self, dmtype="adb", hwid=None, host=None, port=None):
+        '''
+        Returns a device with the specified parameters
+        '''
+        if self.options.verbose:
+            mozdevice.DroidSUT.debug = 4
+
+        if hwid:
+            return mozdevice.DroidConnectByHWID(hwid)
+
+        if dmtype == "adb":
+            if host and not port:
+                port = 5555
+            return mozdevice.DroidADB(packageName=None, host=host,
+                                      port=port)
+        elif dmtype == "sut":
+            if not host:
+                self.parser.error("Must specify host with SUT!")
+            if not port:
+                port = 20701
+            return mozdevice.DroidSUT(host=host, port=port)
+        else:
+            self.parser.error("Unknown device manager type: %s" % type)
 
     def push(self, src, dest):
         if os.path.isdir(src):
@@ -217,7 +240,7 @@ class SUTCli(object):
 
 def cli(args=sys.argv[1:]):
     # process the command line
-    cli = SUTCli(args)
+    cli = DMCli(args)
 
 if __name__ == '__main__':
     cli()
