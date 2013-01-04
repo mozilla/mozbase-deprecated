4f4dfa893adc8da38f1f72e4f9f4111da2dc8e4e
Anhad Jai Singh <ffledgling@gmail.com>
2013-01-04 14:49:29 -0500

    Bug 825904 - Added DocStrings, deprecated get_lan_ip() method, added NetworkError exception class for network errors. r=wlach

--- moznetwork/moznetwork/moznetwork.py
+++ moznetwork/moznetwork/moznetwork.py
@@ -10,26 +10,38 @@ if os.name != 'nt':
     import fcntl
 
 
+class NetworkError(Exception):
+    """Unable to obtain interface or IP"""
+
+
 def _get_interface_list():
+    """Provides a list of available network interfaces
+       as a list of tuples (name, ip)"""
     max_iface = 32  # Maximum number of interfaces(Aribtrary)
     bytes = max_iface * 32
     is_32bit = (8 * struct.calcsize("P")) == 32  # Set Architecture
     struct_size = 32 if is_32bit else 40
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    names = array.array('B', '\0' * bytes)
-    outbytes = struct.unpack('iL', fcntl.ioctl(
-        s.fileno(),
-        0x8912,  # SIOCGIFCONF
-        struct.pack('iL', bytes, names.buffer_info()[0])
-    ))[0]
-    namestr = names.tostring()
-    return [(namestr[i:i + 32].split('\0', 1)[0],
-            socket.inet_ntoa(namestr[i + 20:i + 24]))\
-            for i in range(0, outbytes, struct_size)]
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        names = array.array('B', '\0' * bytes)
+        outbytes = struct.unpack('iL', fcntl.ioctl(
+            s.fileno(),
+            0x8912,  # SIOCGIFCONF
+            struct.pack('iL', bytes, names.buffer_info()[0])
+        ))[0]
+        namestr = names.tostring()
+        return [(namestr[i:i + 32].split('\0', 1)[0],
+                socket.inet_ntoa(namestr[i + 20:i + 24]))\
+                for i in range(0, outbytes, struct_size)]
+
+    except IOError:
+        raise NetworkError('Unable to call ioctl with SIOCGIFCONF')
 
 
-def get_lan_ip():
+def get_ip():
+    """Provides an available network interface address. A
+       NetworkError exception is raised in case of failure."""
     try:
         try:
             ip = socket.gethostbyname(socket.gethostname())
@@ -47,4 +59,11 @@ def get_lan_ip():
                 continue
             else:
                 return ifconfig[1]
-    return ip
+
+    if ip is None:
+        raise NetworkError('Unable to obtain network address')
+
+
+def get_lan_ip():
+    """Deprecated. Please use get_ip() instead."""
+    return get_ip()
