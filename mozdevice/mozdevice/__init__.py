32d9ed23a1a23e43486c71139b1a6ee51d36f6ae
Jeff Hammel <jhammel@mozilla.com>
2013-01-25 17:05:04 -0800

    follow up: Bug 807119 - Update mozbase emulator classes;r=jgriffin

--- mozdevice/mozdevice/__init__.py
+++ mozdevice/mozdevice/__init__.py
@@ -6,5 +6,3 @@ from devicemanager import DMError
 from devicemanagerADB import DeviceManagerADB
 from devicemanagerSUT import DeviceManagerSUT
 from droid import DroidADB, DroidSUT, DroidConnectByHWID
-from emulator import Emulator
-from b2gemulator import B2GEmulator
