a659a23094695b8b5c93c0a540996b64a414c58a
Andrew Halberstadt <halbersa@gmail.com>
2013-05-24 13:38:20 -0400

    Bug 870876 - Create a remote mozrunner class for B2G profile and process creation, r=jgriffin

--- mozrunner/setup.py
+++ mozrunner/setup.py
@@ -6,7 +6,7 @@ import sys
 from setuptools import setup
 
 PACKAGE_NAME = "mozrunner"
-PACKAGE_VERSION = '5.18'
+PACKAGE_VERSION = '5.19'
 
 desc = """Reliable start/stop/configuration of Mozilla Applications (Firefox, Thunderbird, etc.)"""
 
