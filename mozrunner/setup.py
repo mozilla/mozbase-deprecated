c37f3b32d907da5effe1429f8b4e0f50effbf8b2
Andrew Halberstadt <halbersa@gmail.com>
2013-07-26 12:06:25 -0400

    Bump mozrunner for bug 898379

--- mozrunner/setup.py
+++ mozrunner/setup.py
@@ -6,7 +6,7 @@ import sys
 from setuptools import setup
 
 PACKAGE_NAME = "mozrunner"
-PACKAGE_VERSION = '5.22'
+PACKAGE_VERSION = '5.23'
 
 desc = """Reliable start/stop/configuration of Mozilla Applications (Firefox, Thunderbird, etc.)"""
 
