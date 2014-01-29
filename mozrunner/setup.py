028975b142fea95f006129c5ed7acf7c6fe80f38
Henrik Skupin <mail@hskupin.info>
2014-01-29 19:13:47 +0100

    Bug 965339 - Bump mozrunner to 5.32

--- mozrunner/setup.py
+++ mozrunner/setup.py
@@ -6,7 +6,7 @@ import sys
 from setuptools import setup
 
 PACKAGE_NAME = 'mozrunner'
-PACKAGE_VERSION = '5.31'
+PACKAGE_VERSION = '5.32'
 
 desc = """Reliable start/stop/configuration of Mozilla Applications (Firefox, Thunderbird, etc.)"""
 
